0ce022c7b9d55b74650449a281eab3c57a693ad0
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-11-06 11:38:04 +0530

    Update App.vue

--- Chapter06/Exercise6.02/src/App.vue
+++ Chapter06/Exercise6.02/src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link :to="{ name: `messageFeed` }">Message Feed</router-link>
+      <router-link :to="{ name: 'messageFeed' }">Message Feed</router-link>
     </div>
     <router-view/>
   </div>
