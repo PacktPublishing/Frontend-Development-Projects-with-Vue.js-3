161e7ab0cd0b6c1d0cc9adf25b0eb8948a52fecb
Raymond Camden <raymondcamden@gmail.com>
2022-03-23 09:00:29 -0500

    initial

---
+++ Chapter09/Exercise9.01/src/components/AppProfileDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="md:w-1/3 h-64 bg-blue-200 flex p-12">
+  <!-- Profile Card -->
+  <h3 class="font-bold font-lg">{{ formData.name }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+	props: {
+		formData: {
+			type: Object, 
+			default() {
+				return {}
+			}
+		}
+	}
+}
+</script>
---
+++ Chapter09/Exercise9.01/src/components/AppProfileForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="md:w-2/3 flex flex-col p-12 items-center bg-red-200">
+
+  	<!-- Inputs -->
+    <div class="flex flex-col">
+      <label class="flex text-gray-800 mb-2" for="name">Name
+      </label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        class="border-2 border-solid border-blue-200 rounded 
+          px-2 py-1"
+		v-model="name"
+      />
+    </div>
+
+    <div class="flex flex-row mt-12">
+      <button type="submit" @click="submitForm()">Submit</button>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name:''
+		}
+	},
+	methods: {
+		submitForm() {
+			this.$emit('submit', {
+				name: this.name
+			})
+		}
+	}
+}
+</script>
