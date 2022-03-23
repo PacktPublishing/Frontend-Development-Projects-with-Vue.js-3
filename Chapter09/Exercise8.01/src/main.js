858c3e5799c8a309d7384da3d7d7b3355d51d473
Raymond Camden <raymondcamden@gmail.com>
2022-03-23 09:08:40 -0500

    cleanup

--- Chapter09/Exercise8.01/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import '@/assets/css/tailwind.css'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
--- Chapter09/Exercise8.02/src/components/AppHeader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="w-full block p-4 border-b bg-blue-300 border-gray-700">
-    <h2 class="text-xl text-gray-800">Profile Card Generator</h2>
-  </header>
-</template>
