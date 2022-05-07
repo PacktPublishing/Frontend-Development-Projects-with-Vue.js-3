5cf11e1a4348ea29e58841ec393f328b6cb8c0b5
Raymond Camden <raymondcamden@gmail.com>
2022-05-07 08:15:10 -0500

    new ex

---
+++ Chapter10/Exercise10.03/src/components/PreviewColor.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+import { useColorStore } from '@/stores/color';
+const store = useColorStore()
+
+const previewRGB = computed(() => {
+	
+	return { 
+		backgroundColor: store.hex
+	}
+});
+</script>
+
+<template>
+<div>
+	<h2>Preview Color</h2>
+	<div class="previewColor" :style="previewRGB"></div>
+	<p>
+	CSS color string: {{ previewRGB.backgroundColor }}
+	</p>
+</div>
+</template>
+
+<style>
+.previewColor {
+	width: 250px;
+	height: 250px;
+}
+</style>
---
+++ Chapter10/Exercise10.03/src/components/RGBEdit.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useColorStore } from '@/stores/color';
+const store = useColorStore()
+</script>
+
+<template>
+<div>
+	<h2>Edit RGB</h2>
+	<label for="red">Red</label> <input type="range" min="0" max="255" id="red" v-model="store.red"><br/>
+	<label for="green">Green</label> <input type="range" min="0" max="255" id="green" v-model="store.green"><br/>
+	<label for="blue">Blue</label> <input type="range" min="0" max="255" id="blue" v-model="store.blue"><br/>
+
+	<p>
+	<button @click="store.darken()">Darken</button>
+	<button @click="store.lighten()">Lighten</button>
+	</p>
+	
+</div>
+
+
+</template>
+
+<style>
+label {
+	display: inline-block;
+	width: 50px;
+}
+</style>
