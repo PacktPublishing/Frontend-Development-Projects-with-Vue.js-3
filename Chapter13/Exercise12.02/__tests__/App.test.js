49ed3516da12937c5a24998ef5a9772c6e788195
Raymond Camden <raymondcamden@gmail.com>
2022-12-19 08:58:14 -0600

    remove old 13

--- Chapter13/Exercise12.02/__tests__/App.test.js
+++
@@ -1,7 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import App from '../src/App.vue'
-
-test('App renders blog title correctly', () => {
-  const wrapper = shallowMount(App)
-  expect(wrapper.text()).toMatch("The Vue.js Workshop Blog")
-})
