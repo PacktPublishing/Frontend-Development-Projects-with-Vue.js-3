9f0f64a855271b7e9b43e395e838a90279b5239b
Maya Shavin <dpnminh@gmail.com>
2023-01-11 10:40:38 +0200

    fix: chapter 7 activity

--- Chapter07/Actitivity7.01/src/views/Messages.vue
+++ Chapter07/Actitivity7.01/src/views/Messages.vue
@@ -3,13 +3,11 @@
     <router-link :to="{ name: 'list' }">List</router-link> |
     <router-link :to="{ name: 'editor' }">Editor</router-link>
     <router-view v-slot="{ Component }">
-      <transition>
-        <component
-          :is="Component"
-          :list="messages"
-          @list:update="addMessage"
-        />
-      </transition>
+      <component
+        :is="Component"
+        :list="messages"
+        @list:update="addMessage"
+      />
     </router-view>
   </div>
 </template>
@@ -19,17 +17,13 @@ import { useRoute } from "vue-router";
 import { reactive } from "vue";
 
 const route = useRoute();
-const messages = reactive(route.meta.messages || []);
 const props = defineProps({
-  list: {
-    type: Array,
-    default: () => [],
-  },
   currentLayout: {
     type: Object,
     default: () => DefaultLayout,
   },
 });
+const messages = reactive(route.meta.messages || []);
 
 const emits = defineEmits(["update:currentLayout"]);
 emits('update:currentLayout', DefaultLayout);
